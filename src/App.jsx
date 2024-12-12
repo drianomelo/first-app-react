34de1fa92b2ea7594d5f83614ad25b5ad0518427
Adriano <adrianosenny@gmail.com>
2024-12-12 13:41:21 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { CORE_CONCEPTS as cards } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  function handleSelect(selectedButton) {
+
+    console.log("TENIS GRANDE" + selectedButton);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {cards.map((card, i) => {
+              return <CoreConcept key={i} {...card} />;
+            })}
+          </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
